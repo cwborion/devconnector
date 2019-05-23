da50e1fb39ee93fe7af0f1f4cad6c958158054b8
Caleb Brenner <calebwill91@gmail.com>
2019-05-23 03:52:30 -0500

    moving along through tutorial, making comments for educational purposes

---
+++ middleware/auth.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function(req, res, next) {
+  // Get token from header
+  const token = req.header('x-auth-token');
+
+  // Check if not token
+  if (!token) {
+    return res.status(401).json({ msg: 'No token, authorization denied' });
+  }
+
+  // Verify token
+  try {
+    // jwt.verify returns the decoded payload, which is why decoded.user is
+    // essentially "payload.user", containing the {user: {id: user.id}} object
+    const decoded = jwt.verify(token, config.get('jwtSecret'));
+
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: 'Token is not valid' });
+  }
+};
